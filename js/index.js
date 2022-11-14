a05357b1d2665ad9df28a7650c4472b526bcd823
aym-ie <awyee@ucsd.edu>
2022-11-14 01:48:30 -0800

    2

--- js/index.js
+++ js/index.js
@@ -1,19 +1,32 @@
-/*
-window.addEventListener('load', () => {
-    document.getElementById('yes').addEventListener('click', () => {
-        var decOpt = document.getElementById('hidden');
-        var yncheck = document.getElementById('yes');
-    
-        if (yncheck.checked == true) {
-            decOpt.style.display = 'block';
-            document.getElementById('numOfDec').setAttribute('required', true);
+window.addEventListener('load',() => {
+    var leftpara = document.getElementById('leftparenthesis');
+    var rightpara = document.getElementById('rightparenthesis');
+    leftpara.addEventListener('click', () => {
+        if (leftpara.innerText === "(") {
+            leftpara.innerText = "[";
         } else {
-            decOpt.style.display = 'none';
-            document.getElementById('numOfDec').setAttribute('required', false);
+            leftpara.innerText = "(";
+        }
+    });
+    rightpara.addEventListener('click', () => {
+        if (rightpara.innerText === ")") {
+            rightpara.innerText = "]";
+        } else {
+            rightpara.innerText = ")";
         }
     });
 });
-*/
+
+window.addEventListener('load',() => {
+    var decYes = document.getElementById('ynDec');
+    decYes.addEventListener('click', () => {
+        if (decYes.innerText === "→.00") {
+            decYes.innerText = "←.00";
+        } else {
+            decYes.innerText = "→.00";
+        }
+    });
+});
 
 window.addEventListener('load',() => {
     document.getElementById('submission').addEventListener('click', () => {
@@ -21,12 +34,10 @@ window.addEventListener('load',() => {
         let minbound = parseInt(document.getElementById('minNum').value);
         let maxbound = parseInt(document.getElementById('maxNum').value);
 
-        var minExclude = document.getElementById('exclusiveMin').checked;
-        var maxExclude = document.getElementById('exclusiveMax').checked;
+        var minExclude = document.getElementById('leftparenthesis').innerText;
+        var maxExclude = document.getElementById('rightparenthesis').innerText;
 
-        var decYes = document.getElementById('yes').checked;
-
-        // var decAmount = parseInt(document.getElementById('numOfDec').value);
+        var decYes = document.getElementById('ynDec').innerText;
 
         var output = document.getElementById('resultant')
 
@@ -36,28 +47,28 @@ window.addEventListener('load',() => {
         else if (minbound > maxbound) {
             alert("Make sure the minimum boundary is less than or equal to the maximum boundary!");
         }
-        else if (decYes == true) {
+        else if (decYes === "←.00") {
             minbound = Math.ceil(minbound);
             maxbound = Math.floor(maxbound);
             
-            output.innerHTML = Math.random() * (maxbound - minbound) + minbound;
+            output.innerText = Math.random() * (maxbound - minbound) + minbound;
             output.style.display = 'block';
         } 
         else {
-            if (minExclude != true && maxExclude != true) { // inclusive inclusive
-                output.innerHTML = Math.floor(Math.random() * (maxbound - minbound + 1) + minbound);
+            if (minExclude === "[" && maxExclude === "]") { // inclusive inclusive
+                output.innerText = Math.floor(Math.random() * (maxbound - minbound + 1) + minbound);
                 output.style.display = 'block';
             }
-            else if (minExclude == true && maxExclude != true) { // exclusive inclusive
-                output.innerHTML = Math.floor(Math.random() * (maxbound - minbound) + (minbound + 1));
+            else if (minExclude === "(" && maxExclude === "]") { // exclusive inclusive
+                output.innerText = Math.floor(Math.random() * (maxbound - minbound) + (minbound + 1));
                 output.style.display = 'block';
             }
-            else if (minExclude != true && maxExclude == true) { // inclusive exclusive
-                output.innerHTML = Math.floor(Math.random() * (maxbound - minbound) + minbound);
+            else if (minExclude === "[" && maxExclude === ")") { // inclusive exclusive
+                output.innerText = Math.floor(Math.random() * (maxbound - minbound) + minbound);
                 output.style.display = 'block';
             }
-            else /* (minExclude == true && maxExclude == true)  */ { // exclusive exclusive
-                output.innerHTML = Math.floor(Math.random() * (maxbound - minbound - 1) + (minbound + 1));
+            else /* minExclude === "(" && maxExclude === ")"  */ { // exclusive exclusive
+                output.innerText = Math.floor(Math.random() * (maxbound - minbound - 1) + (minbound + 1));
                 output.style.display = 'block';
             }
         }
